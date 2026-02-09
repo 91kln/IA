417e98dbc60d97958a866c11fed341721b491413
91kln <gryxzb@gmail.com>
2026-02-09 22:28:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown("<style>.stApp { background-color: #131314; color: #ffffff; }</style
 
 # Tes Clés
 GROQ_KEY = "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"
-TAVILY_KEY = "METS_TA_CLE_TAVILY_ICI" # <--- METS TA CLÉ ICI
+TAVILY_KEY = "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" # <--- METS TA CLÉ ICI
 
 client = Groq(api_key=GROQ_KEY)
 tavily = TavilyClient(api_key=TAVILY_KEY)
@@ -91,3 +91,4 @@ if prompt := st.chat_input("Pose n'importe quelle question..."):
         messages_actuels.append({"role": "assistant", "content": reponse_ia})
         st.session_state.tous_chats[st.session_state.chat_actuel] = messages_actuels
         sauvegarder_tous_les_chats(st.session_state.tous_chats)
+
